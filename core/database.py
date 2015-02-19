57c8f2f1d1f5d3d93d0fae50328c9dd91f806672
meyt <pasd3000@gmail.com>
2015-02-20 02:50:13 +0330

    update 0.2

--- core/database.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-class Database:
-    def __init__(self, path):
-        print ""
-
--- main.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import logging
-from core import app
-
-logging.basicConfig(level=logging.DEBUG)
-main = app.App()
-main.run()
